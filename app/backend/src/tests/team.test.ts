cbb8fddba80ccf22d00bffe85a05f802fd7fe505
Gabriel Franca <gabrielcfranca@hotmail.com>
2022-10-12 12:41:56 -0300

    add testes

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -6,6 +6,7 @@ import chaiHttp = require('chai-http');
 import { app } from '../app';
 import Team from '../database/models/TeamModel';
 import { mockTeams, mockTeamsId } from './mocks/teamMocks';
+import { error } from 'console';
 
 chai.use(chaiHttp);
 
@@ -36,32 +37,77 @@ describe('get /Teams', () => {
     });
   });
 });
-  describe('get /Teams/id:', () => {
 
-    describe('Verifica se os times estão sendo encontrados pelo Id', () => {
-      before(async () => {
-        sinon
-          .stub(Team, "findByPk")
-          .resolves(
-            mockTeamsId as Team
-          );
-      });
-    
-      after(()=>{
-        (Team.findByPk as sinon.SinonStub).restore();
-      })
-    
-      it('Retorna um time com status 200', async () => {
-        const teamId = 2;
-        const chaiHttpResponse = await chai
-           .request(app)
-           .get('/teams/${teamId}');
+describe('get /Teams/id:', () => {
+
+  describe('Verifica se os times estão sendo encontrados pelo Id', () => {
+    before(async () => {
+      sinon
+        .stub(Team, "findByPk")
+        .resolves(
+          mockTeamsId as Team
+        );
+    });
   
-           expect(chaiHttpResponse.body).to.be.deep.equal({ id: teamId, teamName: "Bahia" });
-           expect(chaiHttpResponse.status).to.be.equal(200);
-      });
+    after(()=>{
+      (Team.findByPk as sinon.SinonStub).restore();
     })
+  
+    it('Retorna um time com status 200', async () => {
+      const teamId = 2;
+      const chaiHttpResponse = await chai
+          .request(app)
+          .get('/teams/${teamId}');
+
+          expect(chaiHttpResponse.body).to.be.deep.equal({ id: teamId, teamName: "Bahia" });
+          expect(chaiHttpResponse.status).to.be.equal(200);
+    });
+  })
+  describe ('Quando o id: é inválido', async () => {
+    before(async () => {
+      sinon
+      .stub(Team, "findByPk")
+      .resolves(
+        null as unknown as Team
+      );
   });
+
+  after(()=>{
+    (Team.findByPk as sinon.SinonStub).restore();
+  })
+
+  it('Informa que o team no foi encontrado e retorna o erro 404', async () => {
+    const teamId = 50;
+    const chaiHttpResponse = await chai
+      .request(app)
+      .get('/teams/${teamId}');
+
+      expect(chaiHttpResponse.body).to.be.deep.equal({ message: "Team not found" })
+      expect(chaiHttpResponse.status).to.be.equal(404);
+  })
+  })
+  describe ('Quando ocorre um erro interno', async () => {
+    before(async () => {
+      sinon
+      .stub(Team, "findAll")
+      .rejects();
+  });
+
+  after(()=>{
+    (Team.findAll as sinon.SinonStub).restore();
+  })
+
+  it('Informa o erro 500 com a error.message', async () => {
+    const teamId = 50;
+    const chaiHttpResponse = await chai
+      .request(app)
+      .get('/teams');
+
+      // expect(chaiHttpResponse.body).to.be.deep.equal({ message: Error })
+      expect(chaiHttpResponse.status).to.be.equal(500);
+  })
+  })
+});
 
   // describe('Verifica um email é válido', () => {
   //   before(async () => {
